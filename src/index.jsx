4215e609d8ce1f379925703f659a908cb59cd3cd
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-08-07 19:55:08 +0200

    Update routes

--- src/index.jsx
+++ src/index.jsx
@@ -4,18 +4,24 @@ import { BrowserRouter } from 'react-router-dom'
 import { Routes, Route } from 'react-router-dom'
 import './utils/style/index.css'
 import Home from './pages/Home'
+import Logement from './pages/Logement'
+import About from './pages/About'
 import NotFound from './pages/NotFound'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
 	<BrowserRouter>
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Home/>}/>
-				<Route path="*" element={<NotFound/>}/>
-			</Routes>
-		</div>
+		<Header />
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/logement:id" element={<Logement />} />
+			<Route path="/about" element={<About />} />
+			<Route path="*" element={<NotFound />} />
+		</Routes>
+		<Footer/>
 	</BrowserRouter>
 )
 
