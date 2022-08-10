b7264d709e289d858d3bc6f1f262d8bf93e021a1
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-08-10 11:44:28 +0200

    more modulable component

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,34 +1,55 @@
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import useViewport from '../utils/Hooks/useViewport'
+import Arrow from './Arrows'
 import Img from './Img'
 
-export default function Banner({ txt, img }) {
-
-	const { viewportWidth } = useViewport()
+export default function Banner({ txt, src, height, gradient, arrows }) {
 
 	const bannerImgStyle = {
-		height: viewportWidth < 769 && '14rem',
+		height,
 		borderRadius: '25px',
 		width: '100%',
-		filter: 'brightness(0.75)',
 	}
 
 	return (
+		
 		<ImgContainer>
-			<Img src={img} style={bannerImgStyle} alt="Banner" cover/>
+			{gradient ?
+				<GradientOverlay>
+					<Img src={src} style={bannerImgStyle} alt="Banner" filter="grayscale(.3)" cover/>
+				</GradientOverlay>
+			:
+			<Img src={src} style={bannerImgStyle} alt="Banner" cover/>
+			}
 			<ImgLabel>{txt}</ImgLabel>
+			{arrows && <Arrow/>}
 		</ImgContainer>
+		
+		
 	)
 }
-
+const GradientOverlay = styled.div`
+	&::after {
+		content: '';
+		width: 100%;
+		height: 10rem;
+		position: absolute;
+		top: 0;
+		left: 0;
+		background-image: linear-gradient(rgba(0, 0, 0, 0.2), transparent);
+	}
+`
 const ImgContainer = styled.section`
 	position: relative;
+	border-radius: 25px;
+    overflow: hidden;
+
 `
 
 const ImgLabel = styled.h1`
 	color: white;
 	font-size: 2.3rem;
+	font-weight: 600;
 	position: absolute;
 	top: 50%;
 	text-align: center;
@@ -44,5 +65,7 @@ const ImgLabel = styled.h1`
 
 Banner.propTypes = {
 	txt: PropTypes.string,
-	img: PropTypes.string.isRequired,
+	src: PropTypes.string.isRequired,
+	height: PropTypes.string,
+	gradient: PropTypes.bool,
 }
