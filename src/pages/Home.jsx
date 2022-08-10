78f8e913dddadbf842c254d2900591c5417f78fe
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-08-10 11:47:25 +0200

    HousingContainer responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,40 @@
 import { Container } from "../Components/Container"
-import Hero from "../Components/Hero"
-import HomeHero from '../assets/hero.jpg'
-
+import Banner from "../Components/Banner"
+import HomeBanner from '../assets/home-banner.jpg'
+import styled from "styled-components"
+import Card from "../Components/Card"
+import data from '../data/logements.json'
+import StyledLink from "../Components/StyledLink"
 
 export default function Home() {
-
 	return (
 		<Container>
-			<Hero txt="Chez vous, partout&nbsp;et&nbsp;ailleurs" img={HomeHero} />
-			<h1>Home</h1>
+			<Banner txt="Chez vous, partout&nbsp;et&nbsp;ailleurs" src={HomeBanner} height="10rem" gradient/>
+			<HousingsContainer>
+				{data.map(house => 
+					<StyledLink to={`/logement-${house.id}`} key={`/logement-${house.id}`} state={house} >
+						<Card title={house.title} key={house.id} background={house.cover} />
+					</StyledLink>
+				)}
+			</HousingsContainer>
+
 		</Container>
 	)
 }
+
+const HousingsContainer = styled.section`
+	background-color: #F6F6F6;
+	border-radius: 25px;
+	margin-top: 1.5rem;
+	padding: 2rem;
+	height: auto;
+	display: grid;
+	grid-template-columns: repeat( auto-fit, minmax(350px, 1fr) );
+	grid-gap: 2rem;
+	
+	@media (max-width: 860px) {
+		grid-template-columns: repeat(1,minmax(0,1fr));
+		padding: 0;
+		background-color: #fff;
+	}
+`
